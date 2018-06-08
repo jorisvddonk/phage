0b1a0ca7bacc187764f2b5870695dd2e6a157ba7
joris <jorisvddonk@gmail.com>
2018-06-08 19:30:19 +0200

    Improved camera

--- src/components.js
+++ src/components.js
@@ -3,7 +3,7 @@ AFRAME.registerComponent("follow-camera", {
     var cam = document.getElementById("camera").parentElement;
     var pos = cam.getAttribute("position");
     pos.x = this.el.object3D.position.x;
-    //pos.y = this.el.object3D.position.y + 1.6;
+    pos.y = this.el.object3D.position.y * 0.5;
     pos.z = this.el.object3D.position.z + 4;
     cam.setAttribute("position", pos);
   }
