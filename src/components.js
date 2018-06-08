6d4c7996fcd530b2e547c1dc5bd0538a4d169dce
joris <jorisvddonk@gmail.com>
2018-06-08 19:14:01 +0200

    Fixed lighting, added randomly generated terrain

--- src/components.js
+++ src/components.js
@@ -9,6 +9,68 @@ AFRAME.registerComponent("follow-camera", {
   }
 });
 
+AFRAME.registerComponent("dirlight", {
+  init: function() {
+    var scene = this.el.sceneEl.object3D;
+    var dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.position.set(-1, 1, 0);
+    dirLight.position.multiplyScalar(30);
+    scene.add(dirLight);
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize.width = 1024 * 4;
+    dirLight.shadow.mapSize.height = 1024 * 4;
+    var d = 50;
+    dirLight.shadow.camera.left = -d;
+    dirLight.shadow.camera.right = d;
+    dirLight.shadow.camera.top = d;
+    dirLight.shadow.camera.bottom = -d;
+    dirLight.shadow.camera.far = 500;
+    dirLight.shadow.bias = -0.00001;
+    scene.add(dirLight);
+  }
+});
+
+var SimplexNoise = require("simplex-noise");
+AFRAME.registerComponent("terrain", {
+  init: function() {
+    var simplex = new SimplexNoise();
+    this.geometry = new THREE.Geometry();
+    var SIZE = 100;
+
+    for (var y = 0; y < SIZE; y++) {
+      for (var x = 0; x < SIZE; x++) {
+        this.geometry.vertices.push(
+          new THREE.Vector3(x, y, simplex.noise2D(x, y) * 0.2)
+        );
+      }
+    }
+
+    for (var y = 0; y < SIZE - 1; y++) {
+      for (var x = 0; x < SIZE - 1; x++) {
+        var i = x + y * SIZE;
+        var ij = x + (y + 1) * SIZE;
+        this.geometry.faces.push(new THREE.Face3(i, i + 1, ij));
+        this.geometry.faces.push(new THREE.Face3(ij, i + 1, ij + 1));
+      }
+    }
+
+    this.geometry.translate(SIZE * -0.5, SIZE * -0.5, 0);
+    this.geometry.scale(0.5, 0.5, 0.5);
+
+    this.material = new THREE.MeshStandardMaterial({
+      roughness: 1,
+      metalness: 0,
+      flatShading: true,
+      color: "#383"
+    });
+    this.geometry.computeVertexNormals();
+    this.geometry.computeFaceNormals();
+    this.geometry.computeBoundingSphere();
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.el.setObject3D("mesh", this.mesh);
+  }
+});
+
 AFRAME.registerComponent("spaceship-model", {
   init: function() {
     this.geometry = new THREE.Geometry();
