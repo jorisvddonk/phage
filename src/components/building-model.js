33cec37eeae39d86b8506f9798b3396bb135981a
joris <jorisvddonk@gmail.com>
2018-06-12 22:14:28 +0200

    The building element now has multiple meshes on it, instead of multiple child elements each with its own mesh.
    This makes it a lot easier to do hit detection on objects composed of multiple meshes, as we don't have to navigate the element tree to figure out if a hit was on a particular element of a particular type.

--- src/components/building-model.js
+++ src/components/building-model.js
@@ -1,12 +1,12 @@
 AFRAME.registerComponent("building-model-bottom", {
   init: function() {
-    this.geometry = new THREE.Geometry();
+    this.geometry_bottom = new THREE.Geometry();
 
     var addVec = (rad, dist, height) => {
       if (!height) {
         height = 0;
       }
-      this.geometry.vertices.push(
+      this.geometry_bottom.vertices.push(
         new THREE.Vector3(Math.sin(rad) * dist, height, Math.cos(rad) * dist)
       );
     };
@@ -14,16 +14,16 @@ AFRAME.registerComponent("building-model-bottom", {
     addVec((Math.PI / 1.5) * 1, 0.75, 0);
     addVec((Math.PI / 1.5) * 2, 0.75, 0);
     addVec(0, 0, 3);
-    this.geometry.faces.push(new THREE.Face3(0, 1, 2));
-    this.geometry.faces.push(new THREE.Face3(0, 1, 3));
-    this.geometry.faces.push(new THREE.Face3(1, 2, 3));
-    this.geometry.faces.push(new THREE.Face3(2, 0, 3));
+    this.geometry_bottom.faces.push(new THREE.Face3(0, 1, 2));
+    this.geometry_bottom.faces.push(new THREE.Face3(0, 1, 3));
+    this.geometry_bottom.faces.push(new THREE.Face3(1, 2, 3));
+    this.geometry_bottom.faces.push(new THREE.Face3(2, 0, 3));
 
-    this.geometry.faces.forEach(function(face, i) {
+    this.geometry_bottom.faces.forEach(function(face, i) {
       face.color.setRGB(0.6, 0.8, 0.9);
     });
 
-    this.geometry.scale(0.2, 0.2, 0.2);
+    this.geometry_bottom.scale(0.2, 0.2, 0.2);
 
     this.material = new THREE.MeshStandardMaterial({
       side: THREE.DoubleSide,
@@ -32,23 +32,23 @@ AFRAME.registerComponent("building-model-bottom", {
       flatShading: true,
       vertexColors: THREE.FaceColors
     });
-    this.geometry.computeVertexNormals();
-    this.geometry.computeFaceNormals();
-    this.geometry.computeBoundingSphere();
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.el.setObject3D("mesh", this.mesh);
+    this.geometry_bottom.computeVertexNormals();
+    this.geometry_bottom.computeFaceNormals();
+    this.geometry_bottom.computeBoundingSphere();
+    var mesh = new THREE.Mesh(this.geometry_bottom, this.material);
+    this.el.setObject3D("meshBottom", mesh);
   }
 });
 
 AFRAME.registerComponent("building-model-top", {
   init: function() {
-    this.geometry = new THREE.Geometry();
+    this.geometry_top = new THREE.Geometry();
 
     var addVec = (rad, dist, height) => {
       if (!height) {
         height = 0;
       }
-      this.geometry.vertices.push(
+      this.geometry_top.vertices.push(
         new THREE.Vector3(Math.sin(rad) * dist, height, Math.cos(rad) * dist)
       );
     };
@@ -66,26 +66,26 @@ AFRAME.registerComponent("building-model-top", {
     addVec(Math.PI / 6 + (Math.PI / 3) * 3, 0.4, 3.5);
     addVec(Math.PI / 6 + (Math.PI / 3) * 4, 0.4, 3.5);
     addVec(Math.PI / 6 + (Math.PI / 3) * 5, 0.4, 3.5);
-    this.geometry.faces.push(new THREE.Face3(0, 1, 2));
-    this.geometry.faces.push(new THREE.Face3(0, 1, 7));
-    this.geometry.faces.push(new THREE.Face3(0, 2, 7));
-    this.geometry.faces.push(new THREE.Face3(1, 2, 7));
+    this.geometry_top.faces.push(new THREE.Face3(0, 1, 2));
+    this.geometry_top.faces.push(new THREE.Face3(0, 1, 7));
+    this.geometry_top.faces.push(new THREE.Face3(0, 2, 7));
+    this.geometry_top.faces.push(new THREE.Face3(1, 2, 7));
 
-    this.geometry.faces.push(new THREE.Face3(0, 3, 4));
-    this.geometry.faces.push(new THREE.Face3(0, 3, 9));
-    this.geometry.faces.push(new THREE.Face3(0, 4, 9));
-    this.geometry.faces.push(new THREE.Face3(3, 4, 9));
+    this.geometry_top.faces.push(new THREE.Face3(0, 3, 4));
+    this.geometry_top.faces.push(new THREE.Face3(0, 3, 9));
+    this.geometry_top.faces.push(new THREE.Face3(0, 4, 9));
+    this.geometry_top.faces.push(new THREE.Face3(3, 4, 9));
 
-    this.geometry.faces.push(new THREE.Face3(0, 5, 6));
-    this.geometry.faces.push(new THREE.Face3(0, 5, 11));
-    this.geometry.faces.push(new THREE.Face3(0, 6, 11));
-    this.geometry.faces.push(new THREE.Face3(5, 6, 11));
+    this.geometry_top.faces.push(new THREE.Face3(0, 5, 6));
+    this.geometry_top.faces.push(new THREE.Face3(0, 5, 11));
+    this.geometry_top.faces.push(new THREE.Face3(0, 6, 11));
+    this.geometry_top.faces.push(new THREE.Face3(5, 6, 11));
 
-    this.geometry.faces.forEach(function(face, i) {
+    this.geometry_top.faces.forEach(function(face, i) {
       face.color.setRGB(0.9, 0.6, 0.7);
     });
 
-    this.geometry.scale(0.2, 0.2, 0.2);
+    this.geometry_top.scale(0.2, 0.2, 0.2);
 
     this.material = new THREE.MeshStandardMaterial({
       side: THREE.DoubleSide,
@@ -94,13 +94,13 @@ AFRAME.registerComponent("building-model-top", {
       flatShading: true,
       vertexColors: THREE.FaceColors
     });
-    this.geometry.computeVertexNormals();
-    this.geometry.computeFaceNormals();
-    this.geometry.computeBoundingSphere();
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.el.setObject3D("mesh", this.mesh);
+    this.geometry_top.computeVertexNormals();
+    this.geometry_top.computeFaceNormals();
+    this.geometry_top.computeBoundingSphere();
+    var mesh = new THREE.Mesh(this.geometry_top, this.material);
+    this.el.setObject3D("meshTop", mesh);
   },
   tick: function(time, timeDelta) {
-    this.el.object3D.rotation.y += (timeDelta / 1000) * 6;
+    this.el.getObject3D("meshTop").rotation.y += (timeDelta / 1000) * 6;
   }
 });
--- src/components/building.js
+++ src/components/building.js
@@ -1,11 +1,7 @@
 AFRAME.registerComponent("building", {
   init: function() {
-    var top = document.createElement("a-entity");
-    top.setAttribute("building-model-top", "");
-    var bottom = document.createElement("a-entity");
-    bottom.setAttribute("building-model-bottom", "");
-    this.el.appendChild(top);
-    this.el.appendChild(bottom);
+    this.el.setAttribute("building-model-top", "");
+    this.el.setAttribute("building-model-bottom", "");
     this.el.setAttribute("place-on-ground", "");
   }
 });
