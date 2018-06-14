3426305b3114d8df5e1c8405770d8f7c65926eda
joris <jorisvddonk@gmail.com>
2018-06-14 21:43:33 +0200

    in VR, show the spaceship in the center of the user's room so that they can walk around it

--- src/components/follow-camera.js
+++ src/components/follow-camera.js
@@ -1,10 +1,16 @@
 AFRAME.registerComponent("follow-camera", {
+  init: function() {
+    this.inVR = false;
+    this.el.sceneEl.addEventListener("enter-vr", () => {
+      this.inVR = true;
+    });
+  },
   tick: function() {
     var cam = document.getElementById("camera").parentElement;
     var pos = cam.getAttribute("position");
     pos.x = this.el.object3D.position.x;
     pos.y = this.el.object3D.position.y * 0.5;
-    pos.z = this.el.object3D.position.z + 4;
+    pos.z = this.el.object3D.position.z + (this.inVR ? 0 : 4);
     cam.setAttribute("position", pos);
   }
 });
