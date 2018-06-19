93ade9ed38b7ca446fa4fbceaa7e36b81b9de371
joris <jorisvddonk@gmail.com>
2018-06-19 22:15:31 +0200

    Allow specifying world seed via query string parameter

--- src/systems/terrain-data.js
+++ src/systems/terrain-data.js
@@ -1,8 +1,16 @@
+var url = require("url");
 var SimplexNoise = require("simplex-noise");
 AFRAME.registerSystem("terrain-data", {
   init: function() {
-    console.log("Terrain data system loaded!");
-    this.simplex = new SimplexNoise();
+    var parsedURL = url.parse(document.location.toString(), true);
+    var seed = undefined;
+    if (parsedURL.query.seed) {
+      seed = parsedURL.query.seed;
+    }
+    console.log(
+      "Terrain data system loaded! " + (seed ? "Seed : " + seed : "")
+    );
+    this.simplex = new SimplexNoise(seed);
   },
   getHeightForCoordinates: function(x, y) {
     return this.simplex.noise2D(x, y);
